41cb3620a5e0b79f6600b252ae4e4dc95c31e4ce
iggoncharov <56764427+iggoncharov@users.noreply.github.com>
2021-11-30 17:36:07 +0300

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -1,29 +1,76 @@
-import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 import lxml
+import random
+import time
+import os
+
+def parser(url: str, page_start: int, page_count: int, bool_table: bool = True, bool_pictures: bool = True):
+    """
+    :param url: Ссылка на страницу
+    :param page_start: Начальная страница для парсинга
+    :param: page_count: Количество страниц для парсинга
+    :param: bool_table: Вывести таблицу в виде data_frame
+    :param: bool_pictures: Сохранить картинки или нет
+    """
+    link = 'https://www.mr-group.ru'
+    home = {'id_flat': [],
+            'Squre': [],
+            'Floor': [],
+            'Price': [],
+            'Price per square': [],
+            'Rooms': [],
+            'Frame': []
+            }
+
+    # папка для хранения картинок
+    if bool_pictures:
+        name_folder = 'pictures'
+        os.mkdir(name_folder)
+
+    id_flat = 1
+    for page in range(page_start, page_start + page_count):
+        url_page = url + str(page)
+        response = requests.get(url_page)
+        soup = BeautifulSoup(response.text, "lxml")
+        house_data = soup.find_all('div', class_='catalog-item__wrap')
+
+        for flat in house_data:
+            home['Squre'].append(flat.find_all("div", class_="catalog-item__title _hover")[2].text.split()[2])
+            home['Floor'].append(flat.find_all("div", class_="catalog-item__title _hover")[1].text.replace(" этаж", ''))
+            home['Price'].append(
+                int(flat.find("div", class_="catalog-item__title _price _hover").text[:-1].replace('\xa0', '')))
+            home['Price per square'].append(
+                int(flat.find("div", class_="catalog-item__subtitle _area").text.replace('\xa0', '').split()[0]))
+            home['Rooms'].append(flat.find_all("div", class_="catalog-item__title _hover")[2].text.split()[0])
+            home['Frame'].append(flat.find_all("div", class_="catalog-item__title _hover")[0].text)
+            home['id_flat'].append(id_flat)
 
 
-url = 'https://www.mr-group.ru/catalog/apartments/?project=45589&project_type=apartments&view_mode=list&building%5B%5D=all&floor%5B%5D=all&renovation%5B%5D=all&availability_renovation=all&sort=PROPERTY_IS_ENABLED_DESC&page=1'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "lxml")
-house_data = soup.find_all('div', class_ = 'catalog-item__wrap')
+            #картинки
+            if bool_pictures:
+                #ссылка на картинку
+                url_picture = link + flat.select("img")[0].attrs["data-src"]
+                #отправляем запрос на картинку
+                img_bytes = requests.get(url_picture).content
+                with open(f'{name_folder}/{str(id_flat)}.jpg', 'wb') as file:
+                    file.write(img_bytes)
 
-home = {'Площадь': [],
-        'Этаж': [],
-        'Цена': [],
-        'Цена за Квадратный метр': [],
-        'Кол-во комнат': [],
-        'Корпус': []
-       }
+            id_flat += 1
 
-for flat in house_data:
-    home['Площадь'].append(flat.find_all("div", class_ = "catalog-item__title _hover")[2].text.split()[2])
-    home['Этаж'].append(flat.find_all("div", class_ = "catalog-item__title _hover")[1].text.replace(" этаж", ''))
-    home['Цена'].append(int(flat.find("div", class_ = "catalog-item__title _price _hover").text[:-1].replace('\xa0', '')))
-    home['Цена за Квадратный метр'].append(int(flat.find("div", class_ = "catalog-item__subtitle _area").text.replace('\xa0', '').split()[0]))
-    home['Кол-во комнат'].append(flat.find_all("div", class_ = "catalog-item__title _hover")[2].text.split()[0])
-    home['Корпус'].append(flat.find_all("div", class_ = "catalog-item__title _hover")[0].text)
+        #задержка для парсинга одной страницы
+        print(f'Обработалась страница {page}')
+        time_stop = random.uniform(0, 5)
+        time.sleep(time_stop)
 
-data = pd.DataFrame(home)
-print(data)
+    data = pd.DataFrame(home)
+    data.to_csv('data_parsing.csv', sep=';', encoding='utf-8')
+
+    if bool_table:
+        print(data)
+
+    print('Выполнение окончено')
+
+parser(url = 'https://www.mr-group.ru/catalog/apartments/?project=45589&project_type=apartments&view_mode=list&building%5B%5D=all&floor%5B%5D=all&renovation%5B%5D=all&availability_renovation=all&sort=PROPERTY_IS_ENABLED_DESC&page=',
+       page_start = 1, page_count = 2, bool_table = True, bool_pictures = False)
